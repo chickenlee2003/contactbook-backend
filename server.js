401945775999324d39be8c0627cd6a30c7300605
chicken lee <tuankie191103@gmail.com>
2023-08-20 22:56:48 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
+
+async function startServer(){
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Servcer is running on port ${PORT}.`);
+        });
+    } catch  (error){
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+
+startServer();
+
+
+
+
+
+
+
 
 //start sercer
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Servcer is running on port ${PORT}.`);
-});
+
+
